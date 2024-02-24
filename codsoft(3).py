23907d38d337af00fa01c584acd2ebc781054b22
KunchepuKusuma <141536699+KunchepuKusma@users.noreply.github.com>
2024-02-24 20:32:52 +0530

    Add files via upload

---
+++ codsoft(3).py
@@ -0,0 +1,11 @@
+import random
+import string
+length = int(input("Enter The Length of Passwordss: "))
+chars = string.ascii_letters
+chars += string.digits
+chars += string.punctuation
+password = ""
+for i in range(length):
+    password += random.choice(chars)
+password = ''.join([random.choice(chars) for i in range(length)])
+print("Your random password is:", password)
